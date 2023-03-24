677ff176a8dd96a9985a782bf60594e24166caf0
yash.codedrill <yash.codedrill@gmail.com>
2023-03-24 17:26:58 +0530

    main

--- src/common/Cards/TrendingCards.tsx
+++ src/common/Cards/TrendingCards.tsx
@@ -1,20 +1,7 @@
 import React from "react";
 
 const TrendingCards = () => {
-    return (
-        <div className="flex flex-col gap-2 bg-grey-100 h-full">
-            <img src="/Rasol.JPG" className="w-full h-full" />
-            <div className="flex flex-col gap-2 p-2 absolute bottom-0 ">
-                <h2 className="text-primary-700 text-xl ">
-                    India’s most beautiful place all must visit
-                </h2>
-                <p>
-                    This place is situated in Himachal Pradesh Kangra{" "}
-                    <span className="text-primary-700">Read More...</span>
-                </p>
-            </div>
-        </div>
-    );
+    return <img src="/Rasol.JPG" className="w-full h-full object-cover" />;
 };
 
 export default TrendingCards;
--- src/components/Home/Trending.tsx
+++ src/components/Home/Trending.tsx
@@ -6,7 +6,7 @@ const Trending = () => {
     return (
         <div className="mt-2 lg:mt-12">
             <Container>
-                <div className="flex flex-col  gap-2 lg:gap-4 ">
+                <div className="flex flex-col py-8 gap-2 lg:gap-4 ">
                     <h1 className="text-3xl lg:text-5xl">
                         <span className="text-primary-700">Trending</span>{" "}
                         <span>blogs</span>
@@ -16,7 +16,7 @@ const Trending = () => {
                         that you can see the world.
                     </p>
 
-                    <div className="grid grid-rows-2 grid-flow-col gap-4 ">
+                    {/* <div className="grid grid-rows-2 grid-flow-col gap-4 ">
                         <div className="row-span-3 max-h-[656px] overflow-hidden relative">
                             <TrendingCards />
                         </div>
@@ -25,6 +25,20 @@ const Trending = () => {
                         </div>
                         <div className="row-span-2 col-span-2 max-h-[318px] overflow-hidden relative">
                             <TrendingCards />
+                        </div>
+                    </div> */}
+
+                    <div className="flex w-full gap-4 ">
+                        <div className="max-h-[500px]">
+                            <TrendingCards />
+                        </div>
+                        <div className="flex flex-col max-h-[500px] gap-4">
+                            <div className="max-h-[243px]">
+                                <TrendingCards />
+                            </div>
+                            <div className="max-h-[243px]">
+                                <TrendingCards />
+                            </div>
                         </div>
                     </div>
                 </div>
