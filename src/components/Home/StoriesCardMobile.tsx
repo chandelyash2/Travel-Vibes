de1131520a9c5b8d78869fa10c0da174061a0609
Yash Chandel <yashchandel@Yashs-MacBook-Air.local>
2023-05-16 16:13:32 +0530

    fix design issues

--- src/components/Home/StoriesCardMobile.tsx
+++ src/components/Home/StoriesCardMobile.tsx
@@ -1,12 +1,17 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/no-unescaped-entities */
+import { useRouter } from "next/router";
 import React from "react";
 import Title from "src/common/Title";
 
 const StoriesCardMobile = () => {
+    const router =useRouter()
     return (
         <div className="flex flex-col gap-4 lg:hidden">
-            <div className="relative flex flex-col gap-2">
+            <div
+                className="relative flex flex-col gap-2"
+                onClick={() => router.push("/blog/abc")}
+            >
                 <img src="Rasol.JPG" className="max-h-[540px]" alt="stories" />
                 <div className="absolute  bg-mask bg-no-repeat w-full min-h-[160px] bottom-0 left-0 flex flex-col justify-end pb-4 px-4 ">
                     <Title label="India’s most beautiful  place all must visit" />
